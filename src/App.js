bbb74b14a5f26e70b5265335057f258a4e03478b
Alimilla-Abhinandan <abhialimilla@gmail.com>
2024-11-12 00:30:41 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="app-container">
       <h1 className="app-title">Student Management System</h1>
-    <p className="developer-info">Alimilla Abhinandan</p>
+    <p className="developer-info">Developed by Alimilla Abhinandan</p>
       <StudentList />
     </div>
   );
